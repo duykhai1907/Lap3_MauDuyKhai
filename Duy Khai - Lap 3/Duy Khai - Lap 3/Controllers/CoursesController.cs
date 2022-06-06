51f6a028fa0d341fa4618f8c7bea6c5da2a5f838
Khai <Khai@gmail.com>
2022-06-06 10:23:59 +0700

    Update page 38

---
+++ Duy Khai - Lap 3/Duy Khai - Lap 3/Controllers/CoursesController.cs
@@ -0,0 +1,38 @@
+﻿using Duy_Khai___Lap_3.Models;
+using Duy_Khai___Lap_3.Views.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Duy_Khai___Lap_3.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create( CourseViewModel viewModel)
+        {
+            var course = new Course()
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Duy Khai - Lap 3/Duy Khai - Lap 3/ViewModels/CourseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Duy_Khai___Lap_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Duy_Khai___Lap_3.Views.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+
+    }
+}
