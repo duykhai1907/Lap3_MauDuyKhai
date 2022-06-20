89979ec841ef1f57c9d687e127b2cd92a3538700
Kyu0z <duykhaidevfe@gmail.com>
2022-06-20 08:59:26 +0700

    Lap 3 - Buoi 4

--- Duy Khai - Lap 3/Duy Khai - Lap 3/Controllers/HomeController.cs
+++ Duy Khai - Lap 3/Duy Khai - Lap 3/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Duy_Khai___Lap_3.Controllers
 {
@@ -19,8 +20,9 @@ namespace Duy_Khai___Lap_3.Controllers
         public ActionResult Index()
         {
             var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include (c => c.Category)
                 
-       
                 .Where(c => c.DateTime > DateTime.Now);
             var viewModel = new CoursesViewModel
             {
